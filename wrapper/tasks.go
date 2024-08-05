5ec1e7cb621cc541868171ce95caf798257c17bf
Elliot <141949359+webElliot@users.noreply.github.com>
2024-08-05 20:07:46 +0100

    added sleeps to give the host time to startup.

--- wrapper/tasks.go
+++ wrapper/tasks.go
@@ -8,10 +8,12 @@ import (
 )
 
 func StartUp(host string) {
+	time.Sleep(60 * time.Second)
 	tasks.CacheAll(host)
 }
 
 func RefreshCacheHourly(host string) {
+	time.Sleep(60 * time.Second)
 	for {
 		public.LoopUntilCacheUpdated(host)
 		ml.LoopUntilCacheUpdated(host)
